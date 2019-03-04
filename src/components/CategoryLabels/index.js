d60737f0c93214d8a7b529aacb1707cb6f1581b7
moklick <info@moritzklack.com>
2019-03-04 12:48:44 +0100

    style(tooltip): labels

--- src/components/CategoryLabels/index.js
+++ src/components/CategoryLabels/index.js
@@ -1,6 +1,8 @@
 import React, { PureComponent } from 'react';
 import styled from 'styled-components';
 
+import { getColorByCategory } from '~/state/DataUtils';
+
 const CategoryLabelWrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
@@ -8,11 +10,25 @@ const CategoryLabelWrapper = styled.div`
 
 const CategoryLabel = styled.div`
   display: block;
-  font-size: 10px;
+  font-size: ${props => props.theme.fontSizes[0]};
   margin-right: 5px;
   padding: 2px 5px;
   border-radius: 8px;
-  background: #ddd;
+  color: ${props => props.color || '#ddd'};
+  position: relative;
+  padding-left: 12px;
+  line-height: 1;
+
+  &:before {
+    content: '';
+    position: absolute;
+    height: 8px;
+    width: 8px;
+    background: ${props => props.color || '#ddd'};
+    border-radius: 100%;
+    left: 0;
+    top: 3px;
+  }
 `;
 
 class CategoryLabels extends PureComponent {
@@ -20,7 +36,12 @@ class CategoryLabels extends PureComponent {
     return (
       <CategoryLabelWrapper>
         {this.props.categories.map(cat => (
-          <CategoryLabel key={`CategoryLabel__${cat}`}>{cat}</CategoryLabel>
+          <CategoryLabel
+            key={`CategoryLabel__${cat}`}
+            color={getColorByCategory(cat)}
+          >
+            {cat}
+          </CategoryLabel>
         ))}
       </CategoryLabelWrapper>
     );
