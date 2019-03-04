d67910288a8130629e7083af92cd8ac1acee9e7c
moklick <info@moritzklack.com>
2019-03-04 11:46:57 +0100

    feat(select): reset on detail reset

--- src/modules/Sidebar/SidebarFilter/SearchFilter.js
+++ src/modules/Sidebar/SidebarFilter/SearchFilter.js
@@ -11,7 +11,17 @@ const SearchFilterWrapper = styled.div`
   margin-bottom: ${props => props.theme.margin[2]};
 `;
 
+const SearchFilterLabel = styled.div`
+  margin-bottom: 5px;
+`;
+
 class SearchFilter extends PureComponent {
+  componentDidUpdate(prevProps) {
+    if (!this.props.detailData && prevProps.detailData) {
+      this.select.select.clearValue();
+    }
+  }
+
   handleChange(evt) {
     this.props.loadEntryData(evt);
   }
@@ -19,11 +29,17 @@ class SearchFilter extends PureComponent {
   render() {
     return (
       <SearchFilterWrapper>
+        <SearchFilterLabel>
+          Kultureinrichtung suchen
+        </SearchFilterLabel>
         <Select
+          ref={(ref) => { this.select = ref; }}
           options={this.props.options}
           getOptionValue={option => (option.name)}
           getOptionLabel={option => (option.name)}
           onChange={evt => this.handleChange(evt)}
+          placeholder="Namen eingeben ..."
+          classNamePrefix="rs"
         />
       </SearchFilterWrapper>
     );
@@ -32,4 +48,5 @@ class SearchFilter extends PureComponent {
 
 export default connect(state => ({
   options: dataAsArraySelector(state),
+  detailData: state.detailData
 }), Actions)(SearchFilter);
