98ad510c1d72925218793fec199fffb0b9c84ccf
moklick <info@moritzklack.com>
2019-03-28 16:51:52 +0100

    refactor(map): use house as geo marker

--- src/modules/Map/Layers/LocationFilterLayer.js
+++ src/modules/Map/Layers/LocationFilterLayer.js
@@ -1,10 +1,27 @@
 import React, { PureComponent, Fragment } from 'react';
+import styled from 'styled-components';
 import { connect } from 'unistore/react';
 import { Marker } from 'react-mapbox-gl';
-import Place from '@material-ui/icons/Place';
+import Home from '@material-ui/icons/Home';
 
 import Actions from '~/state/Actions';
 import RadiusLayer from './RadiusLayer';
+
+const StyledMarker = styled(Marker)`
+  background: #222;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100%;
+  padding: 3px;
+  opacity: 0.8;
+
+  svg {
+    fill: white;
+    width: 18px;
+    height: 18px;
+  }
+`;
 
 class LocationFilterLayer extends PureComponent {
   render() {
@@ -21,12 +38,12 @@ class LocationFilterLayer extends PureComponent {
           radius={radius}
           fillColor="transparent"
         />
-        <Marker
+        <StyledMarker
           coordinates={coordinates}
           anchor="bottom"
         >
-          <Place />
-        </Marker>
+          <Home />
+        </StyledMarker>
       </Fragment>
     );
   }
