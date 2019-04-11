28bef3b422c9dfaab7ca8d536be6fbf1f6c42b92
g-div <g-div@anche.no>
2019-04-11 15:56:47 +0200

    refactor(package): use idx instead of lodash.get

--- src/state/DataUtils.js
+++ src/state/DataUtils.js
@@ -3,7 +3,6 @@ import turfDistance from '@turf/distance';
 import turfBbox from '@turf/bbox';
 import turfBboxPolygon from '@turf/bbox-polygon';
 import { scaleOrdinal } from 'd3-scale';
-import get from 'lodash.get';
 import idx from 'idx';
 import Store from 'store';
 import museumIcon from '@material-ui/icons/AccountBalance';
@@ -85,8 +84,8 @@ export const sortCategories = (a, b) => {
 };
 
 export const sortData = (sortBy, direction = 'asc') => (aObj, bObj) => {
-  const a = get(aObj, sortBy);
-  const b = get(bObj, sortBy);
+  const a = idx(aObj, _ => _[sortBy]);
+  const b = idx(bObj, _ => _[sortBy]);
   const type = typeof a;
 
   // we always want to display Sonstige as the last category
