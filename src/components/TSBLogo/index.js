bcb813b67917f277a85e07743812070ebd9b5b5a
moklick <info@moritzklack.com>
2019-04-17 12:35:20 +0200

    style(map): use new logo

--- src/components/TSBLogo/index.js
+++ src/components/TSBLogo/index.js
@@ -1,32 +1,31 @@
+import React from 'react';
 import styled from 'styled-components';
-import TSBLogoSrc from '!file-loader!~/../public/images/tsb-logo.svg'; // eslint-disable-line
-import TSBLogoSmallSrc from '!file-loader!~/../public/images/tsb-logo-s.svg'; // eslint-disable-line
+import TSBLogoSrc from '!file-loader!~/../public/images/kulturBdigital-Visual.png'; // eslint-disable-line
 
 const TSBLogo = styled.a.attrs({
   href: 'https://kultur-b-digital.de',
   target: '_blank'
 })`
   display: block;
-  background-image: url(${TSBLogoSmallSrc});
   width: 200px;
   height: 62px;
-  background-size: 100%;
-  background-repeat: no-repeat;
   position: absolute;
-  right: 0px;
-  top: 0px;
+  right: 0;
+  top: 0;
   z-index: 1000;
 
   @media screen and (min-width: 768px) {
-    background-image: url(${TSBLogoSrc});
     width: 230px;
     height: 64px;
   }
 
-  @media screen and (min-width: 1024px) {
-    width: 320px;
-    height: 90px;
+  img {
+    width: 100%;
   }
 `;
 
-export default TSBLogo;
+export default () => (
+  <TSBLogo>
+    <img src={TSBLogoSrc} alt="TSB Logo" />
+  </TSBLogo>
+);
