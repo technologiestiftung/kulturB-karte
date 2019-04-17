cd5db7d6f85e7266661a91b93b5fb96f01b44729
moklick <info@moritzklack.com>
2019-04-17 13:06:16 +0200

    feat(info): add imprint and privacy links

--- src/modules/Sidebar/SidebarInfo/index.js
+++ src/modules/Sidebar/SidebarInfo/index.js
@@ -1,10 +1,20 @@
 import React, { PureComponent, Fragment } from 'react';
+import styled from 'styled-components';
 import ReactMarkdown from 'react-markdown';
 
+import Link from '~/components/Link';
 import markdown from '~/../texts/info.md';
 
 import SidebarTitle from '../SidebarTitle';
 import SidebarText from '../SidebarText';
+
+const ImprintWrapper = styled.div`
+  margin-top: 10px;
+
+  a {
+    font-size: 12px;
+  }
+`;
 
 const Heading = props => <SidebarTitle>{props.children}</SidebarTitle>;
 const Paragraph = props => <SidebarText>{props.children}</SidebarText>;
@@ -20,6 +30,9 @@ class SidebarInfo extends PureComponent {
             paragraph: Paragraph
           }}
         />
+        <ImprintWrapper>
+          <Link target="_blank" href="https://kultur-b-digital.de/impressum">Impressum</Link> · <Link target="_blank" href="https://kultur-b-digital.de/datenschutz">Datenschutz</Link>
+        </ImprintWrapper>
       </Fragment>
     );
   }
