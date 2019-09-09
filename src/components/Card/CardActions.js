c602451d3ade367aff619ca0930f068ae8812881
Christopher Möller <chrtze@gmail.com>
2019-09-09 15:10:04 +0200

    feat(detail): style change button

--- src/components/Card/CardActions.js
+++ src/components/Card/CardActions.js
@@ -4,9 +4,11 @@ import { connect } from 'unistore/react';
 
 import FavIcon from '@material-ui/icons/BookmarkBorder';
 import UnFavIcon from '@material-ui/icons/Bookmark';
+import EditIcon from '@material-ui/icons/Create';
 // import ShareIcon from '@material-ui/icons/Reply';
 
 import Button from '~/components/Button';
+import Link from '~/components/Link';
 
 import Actions from '~/state/Actions';
 
@@ -14,7 +16,6 @@ const CardActionsWrapper = styled.div`
   display: flex;
   justify-content: space-between;
   padding: ${props => props.theme.padding[1]};
-  max-width: 220px;
 `;
 
 const StyledFavIcon = styled(FavIcon)`
@@ -29,6 +30,16 @@ const StyledUnFavIcon = styled(UnFavIcon)`
   margin-top: -2px;
 `;
 
+const StyledEditIcon = styled(EditIcon)`
+  font-size: 15px !important;
+  margin-right: 2px;
+  margin-top: -2px;
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+`;
+
 // const StyledShareIcon = styled(ShareIcon)`
 //   transform: scale(-1, 1);
 //   font-size: 13px !important;
@@ -39,6 +50,7 @@ class CardActions extends PureComponent {
     const { data, toggleFav, favs } = this.props;
     const isFav = favs.includes(data.id);
     const favButtonLabel = isFav ? 'nicht mehr merken' : 'zur Merkliste';
+    const correctionLink = `${config.cms.base}${config.cms.locations}/${data.id}`;
 
     return (
       <CardActionsWrapper>
@@ -49,7 +61,12 @@ class CardActions extends PureComponent {
           {isFav ? <StyledUnFavIcon /> : <StyledFavIcon />}
           <span>{favButtonLabel}</span>
         </Button>
-        { /* <Button><StyledShareIcon /> Teilen</Button> */ }
+        <StyledLink target="_blank" rel="noopener noreferrer" href={correctionLink}>
+          <Button>
+            <StyledEditIcon />
+            <span>Eintrag bearbeiten</span>
+          </Button>
+        </StyledLink>
       </CardActionsWrapper>
     );
   }
--- src/components/Card/Detail.js
+++ src/components/Card/Detail.js
@@ -3,8 +3,6 @@ import styled from 'styled-components';
 import Clear from '@material-ui/icons/Clear';
 
 import RoundButton from '~/components/RoundButton';
-import Button from '~/components/Button';
-import Link from '~/components/Link';
 
 import CardActions from './CardActions';
 import CardWrapper from './CardWrapper';
@@ -92,9 +90,6 @@ class DetailCard extends PureComponent {
         <ScrollWrapper style={{ maxHeight: this.state.maxHeight }}>
           <StyledCardHeader data={data} />
           <CardActions data={data} />
-          <Button>
-            <Link href={`${config.api.base}${config.api.locations}${config.api.edit}`}>Eintrag bearbeiten</Link>
-          </Button>
           <CardDivider />
           <StyledCardBody data={data} />
           <CardNearby data={data.nearby} />
